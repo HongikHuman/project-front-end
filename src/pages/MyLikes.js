148af96d13698bf66945a34a021d61f8311db104
bongsh0112 <bongsh0112@gmail.com>
2021-10-31 13:42:40 +0900

    히스토리 페이지 추가, navbar 명칭 변경

---
+++ src/pages/MyLikes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function MyLikes() {
+    return (
+        <Container className="container">
+            
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    margin: 0 auto;
+    margin-bottom: 150px;
+`;
